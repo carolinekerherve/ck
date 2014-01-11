e826102f50d04a85b0e952846cdbc15f48945ca1
Yann Kerherve <yann.kerherve@gmail.com>
2014-01-11 14:20:43 -0800

    force

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+force=$1
+if [ -n "$force" ]; then
+    opt_force="-a"
+else
+    opt_force=""
+fi
 
 perl -Iextlib/lib/perl5 extlib/bin/ttree -f /dev/null \
-    --define url_base="http://localhost:8000/live/" \
-    -s src -d live -r -v --color \
+    --define url_base="http://yannkerherve.bitbucket.org/live/" \
+    -s src -d live -r -v --color $opt_force \
     --ignore='.*tt' \
     --copy='\.(woff|eot|ttf|css|img|jpe?g|gif|svg|ico|less|js|png)$'
